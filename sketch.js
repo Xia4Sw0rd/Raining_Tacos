2a7a34f9e4341bbfc8db41284b6e12142677e926
Xia4Sw0rd <56092700+Xia4Sw0rd@users.noreply.github.com>
2022-06-24 15:14:00 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,48 +1,76 @@
 var boy,boyImg,boy2,boyImg2;
-var taco,tacoImg,burger,nachos;
+var taco,tacoImg;
 var bomb, bombImg;
-var treasure,redCrystal,purpleDiamond,greenDiamond,lightning,redStar,candy,bluestar,crown;
-var heart;
+var city,cityImg;
+var canvas;
+var score=0;
+
 
 
 
 function preload(){
-    boy = loadImage("Images/boy.png");
-    taco = loadImage ("Images/Taco.jpg");
-    burger = loadImage ("Images/burger.jpg");
-    nachos = loadImage ("Images/Nachos.jpg");
-  //  heart = loadImage();
+   // boy = loadImage("Images/boy.png");
+    boyImg = loadImage("Images/boy.png");
+   // taco = loadImage ("Images/Taco.jpg");
+   // city = loadImage ("sprite_10.png");
+    cityImg = loadAnimation("sprite_10.png");
+
+    bomb = loadImage("Images/bomb-png-46607.png");
+
 }
 
 
 
 function setup(){
-    canvas = createCanvas(windowWidth,windowHeight);
+   
+  canvas = createCanvas(windowWidth,windowHeight);
+
      
       // Moving background
+      city = createSprite(600,200);
+      city.addImage(cityImg);
+      city.scale = 1.3;
      
+   
       
-      //creating the rabbit
-      boy= createSprite(180,340,30,30);
-  //    boy.scale =0.09;
-   //   boy.addImage(boyImg);
+      //creating the boy
+      boy = createSprite(180,340,30,30);
+      boy.addAnimation("boy.png",boyImg);
+      boy.scale =0.90;
+
+
+    //  taco.scale = 0.50;
+    //  bomb.scale = 0.50;
+      
+
+
+    
+   
 }
 
 
 
 function draw() {
-    background(20,22,1);
+    background("black");
     
     edges = createEdgeSprites();
     boy.collide(edges);
     
     //controls (will change to directional keys)
-    boy.x = World.mouseX;
-    
+    if(keyDown("left")){
+      boy.x = boy.x-7;
+     }  
+     if(keyDown("right")){
+      boy.x = boy.x+7; 
+     } 
+    if(spawnTacos.isTouching(boy)){
+      score = score +1;
+    }
+  
    spawnTacos();
    spawnBombs();
-    
-    drawSprites();
+
+   drawSprites();
   }
 
 function windowResized(){
@@ -51,18 +79,17 @@ function windowResized(){
 
 
 // Will Change later
-function spawnTacos(){
-    if (frameCount % 80 == 0){
-  var taco = createSprite(random(50,350),40,10,10);
-  //taco.addImage(tacoImg);
- // taco.scale = 0.03;
-  taco.velocityY = 10; 
-}
-}
 function spawnBombs(){
-  var bomb = createSprite(random(60,350),40,10,10);
-//  bomb.addImage(bombImg);
+    if (frameCount % 80 == 0){
+  var bomb = createSprite(random(windowWidth,windowHeight),40,10,10);
+  bomb.addAnimation(bombImg,"bomb-png-46607.png");
+ // taco.scale = 0.03;
+  bomb.velocityY = 5; 
+}
+}
+function spawnTacos(){
+  var taco = createSprite(random(windowWidth,windowHeight),40,10,10);
+  taco.addAnimation(tacoImg,"Taco.jpg");
  // bomb.scale = 0.03;
-  bomb.velocityY = 5;
-
+  taco.velocityY = 5;
 } 
